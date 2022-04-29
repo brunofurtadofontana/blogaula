b92cd52dcbe261cba4009c9ba8d275c0c0e7b449
Aluno SENAI <Aluno@SN-361232.edu.fiesc.com.br>
2022-04-29 19:22:54 -0300

    Comentarios

--- App/Controller/AdminController.php
+++ App/Controller/AdminController.php
@@ -1,15 +1,22 @@
 <?php
 
+/*
+* Arquivo responsavel por pegar as requisições da view e realizar as chamadas do /model
+* Podendo receber argumentos como insert, delete, update
+*/
+
 class AdminController{
 
 	public function index(){
 		try {
-
+			/*
+			* Função que renderiza a view utilizando o framework TWIG podendo passar dados por parametros para a página html
+			*/
 			$loader = new \Twig\Loader\FilesystemLoader('App/View');
 			$twig = new \Twig\Environment($loader);
 			$template = $twig->load('admin.html');
 			
-			$objPostagem = Postagem::seleciona();
+			$objPostagem = Postagem::seleciona(); //Pega dados de retorno do banco de dados e renderiza na página admin.html
 			$parametros = array();
 			$parametros['postagens'] = $objPostagem;
 
@@ -21,10 +28,21 @@ class AdminController{
 			echo $e->getMessage();
 		}
 	}
-	public function insert(){
+	public function insert(){ //Função que controla o insert no banco de dados 
 		try {
 
-			Postagem::insert($_POST);
+			Postagem::insert($_POST);//manda os dados da view para o método insert dentro do /model/postagem
+			header("Location:?pagina=admin");//retorna para a página admin
+
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+
+	public function delete($paramId){//função que controla a deleção de um registro passando o Id por parametro
+		try {
+
+			Postagem::delete($paramId);//manda os dados para o método delete dentro do /model/postagem
 			header("Location:?pagina=admin");
 
 		} catch (Exception $e) {
@@ -32,21 +50,10 @@ class AdminController{
 		}
 	}
 
-	public function delete($paramId){
+	public function update(){//função que controla a atualização de um registro passando os dados por parametro
 		try {
 
-			Postagem::delete($paramId);
-			header("Location:?pagina=admin");
-
-		} catch (Exception $e) {
-			echo $e->getMessage();
-		}
-	}
-
-	public function update(){
-		try {
-
-			Postagem::update($_POST);
+			Postagem::update($_POST);//manda os dados da view para o método insert dentro do /model/postagem
 			header("Location:?pagina=admin");
 
 		} catch (Exception $e) {
